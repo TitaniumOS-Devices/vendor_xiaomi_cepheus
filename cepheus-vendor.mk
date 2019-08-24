8dc5d209537ddbd4737ec0ce240834a7c523bb45
Akash Srivastava <akashniki@gmail.com>
2019-08-24 11:18:11 +0200

    cepheus: Add displayfeature system blobs

--- cepheus-vendor.mk
+++ cepheus-vendor.mk
@@ -51,6 +51,9 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/cepheus/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
     vendor/xiaomi/cepheus/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
     vendor/xiaomi/cepheus/proprietary/lib64/libdiag_system.so:system/lib64/libdiag_system.so \
+    vendor/xiaomi/cepheus/proprietary/lib64/libdisplayfeature.so:system/lib64/libdisplayfeature.so \
+    vendor/xiaomi/cepheus/proprietary/lib64/libdisplayfeatureservice.so:system/lib64/libdisplayfeatureservice.so \
+    vendor/xiaomi/cepheus/proprietary/lib64/libdisplayservicehidl.so:system/lib64/libdisplayservicehidl.so \
     vendor/xiaomi/cepheus/proprietary/lib64/libdpmctmgr.so:system/lib64/libdpmctmgr.so \
     vendor/xiaomi/cepheus/proprietary/lib64/libdpmfdmgr.so:system/lib64/libdpmfdmgr.so \
     vendor/xiaomi/cepheus/proprietary/lib64/libdpmframework.so:system/lib64/libdpmframework.so \
